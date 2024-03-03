20588dad7c24f437257108ab4311529c69dc4f51
Andreas Papakostas <andreaspkst@gmail.com>
2024-03-03 02:41:37 +0200

    Completed Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { Navbar } from "./components"
+import { Navbar, Hero } from "./components/index.js"
 
 function App() {
 
   return (
     <div className=''>
       <Navbar />
+      <Hero />
     </div>
   )
 }
