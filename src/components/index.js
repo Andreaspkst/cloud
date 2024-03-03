5a9fcb072e831a8334d2d58e0c7f272106775383
Andreas Papakostas <andreaspkst@gmail.com>
2024-03-03 13:09:09 +0200

    Completed Responsive Support Section

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,11 @@
 import Navbar from "./Navbar";
 import Hero from "./Hero";
 import About from "./About";
+import Support from "./Support";
 
 export {
     Navbar,
     Hero,
     About,
-    
+    Support
 }
